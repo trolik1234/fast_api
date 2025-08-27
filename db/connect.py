e125a7c6f86206d2074eaee99a922b63e3e850bf
trolik1234 <166304915+trolik1234@users.noreply.github.com>
2025-08-27 17:39:30 +0300

    Update connect.py

--- db/connect.py
+++ db/connect.py
@@ -1,9 +1,5 @@
-
-
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-
-
 
 DATABASE_URL = "sqlite+aiosqlite:///./trolik.db"
 
